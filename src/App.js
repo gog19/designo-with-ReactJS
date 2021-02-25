6a652cf099f0bae804e17f340142300b78da1109
gayratyuldoshev-go <devinfinitum0@gmail.com>
2021-02-25 09:27:24 +0500

    Change theme format to select version

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Header from './Components/Header/Header';
 import Main from './Components/Main/Main';
-// import Footer from './Components/Footer/Footer';
+import Footer from './Components/Footer/Footer';
 
 function App () {
   return (
     <>  
-      <Header />
+      <Header/>
       <Main />
-      {/* <Footer /> */}
+      <Footer />
     </>
   )
 }
